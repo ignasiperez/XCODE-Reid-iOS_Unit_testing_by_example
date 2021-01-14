550dc9142e69dae6f3997603fbc9f720faa277f3
Ignasi  Perez <perezignasi@gmail.com>
2021-01-14 14:26:20 +0100

    XE-02 LifeCycle -created

---
+++ XE-02 LifeCycle/LifeCycle/MyClass.swift
@@ -0,0 +1,21 @@
+//
+//  MyClass.swift
+//  LifeCycle
+//
+//  Created by ignasiperez.com on 14/01/2021.
+//
+
+import Foundation
+
+class MyClass {
+  
+  func methodOne() {
+    print(">> methodOne")
+  }
+  
+  
+  func methodTwo() {
+    print(">> methodTwo")
+  }
+}
+
