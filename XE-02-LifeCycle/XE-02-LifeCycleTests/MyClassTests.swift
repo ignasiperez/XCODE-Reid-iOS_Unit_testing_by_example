ea0ad221ead52d4a1744e172f422aed185551262
Ignasi  Perez <perezignasi@gmail.com>
2021-01-15 11:52:48 +0100

    XE-02 LifeCycle Observe_Object_Life_Cycles_to_Learn_the_Phases_of_a_Test

--- XE-02-LifeCycle/XE-02-LifeCycleTests/MyClassTests.swift
+++ XE-02-LifeCycle/XE-02-LifeCycleTests/MyClassTests.swift
@@ -5,12 +5,32 @@
 //  Created by ignasiperez.com on 14/01/2021.
 //
 
+@testable import XE_02_LifeCycle
 import XCTest
+
 
 class MyClassTests: XCTestCase {
 
-  func test_zero() {
-    XCTFail("Tests not yet implemented in MyClassTests")
+//  func test_zero() {
+//    XCTFail("Tests not yet implemented in MyClassTests")
+//  }
+  
+  func test_methodOne() {
+    let sut = MyClass()
+    
+    sut.methodOne()
+    
+    XCTFail("Failed, yo")
+  }
+  
+  
+  func test_methodTwo() {
+    let sut = MyClass()
+    
+    sut.methodTwo()
+    
+    // Normally, assert something
+  
   }
 
 }
