3691a0b3f1dfa07259cb766501427cb9f5a20fbf
Ignasi  Perez <perezignasi@gmail.com>
2021-01-19 21:01:13 +0100

    XE-03 CodeCoverage -iter1

--- XE-03-CodeCoverage/XE-03-CodeCoverage/CoveredClass.swift
+++ XE-03-CodeCoverage/XE-03-CodeCoverage/CoveredClass.swift
@@ -5,4 +5,68 @@
 //  Created by ignasiperez.com on 16/01/2021.
 //
 
-import Foundation
+import UIKit
+
+class CoveredClass {
+  
+  
+  
+  // MARK: - 3.8. Cover Statements in a Sequence
+  
+  private(set) var area: Int
+  
+  var width: Int {
+    didSet {
+      area = width * width
+      let color: UIColor = redOrGreen(for: width)
+      drawSquare(width: width, color: color)
+    }
+  }
+  
+  
+  init() {
+    area = 0
+    width = 0
+  }
+  
+  
+  private func redOrGreen(for width: Int) -> UIColor {
+    width % 2 == 0 ? .red : .green
+  }
+  
+  
+  private func drawSquare(width: Int, color: UIColor) {
+    // ...
+  }
+  
+  
+  
+  // MARK: - 3.1. Code Coverage
+  
+  static func max(_ x: Int, _ y: Int) -> Int {
+    if x < y {
+      return y
+    } else {
+      return x
+    }
+  }
+  
+  
+  
+  // MARK: - 3.7. Cover a Loop
+  static func commaSeparated(from: Int,
+                             to: Int) -> String {
+    
+    var result = ""
+    
+    for i in from..<to {
+      result += "\(i),"
+    }
+    
+    result  += "\(to)"
+    
+    return result
+  }
+
+}
+
--- XE-03-CodeCoverage/XE-03-CodeCoverageTests/CoveredClassTests.swift
+++ XE-03-CodeCoverage/XE-03-CodeCoverageTests/CoveredClassTests.swift
@@ -5,28 +5,98 @@
 //  Created by ignasiperez.com on 16/01/2021.
 //
 
+@testable import XE_03_CodeCoverage
 import XCTest
 
 class CoveredClassTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  
+  
+  // MARK: - 3.1. Code Coverage
+  
+  func test_max_with1And2_shouldReturn2() {
+    let result = CoveredClass.max(1, 2)
+    
+//    XCTAssertEqual failed: ("2") is not equal to ("-123")
+//    XCTAssertEqual(result, -123)
+    
+    XCTAssertEqual(result, 2)
+  }
+  
+  
+  func test_max_with3And2_shouldReturn3() {
+    let result = CoveredClass.max(3, 2)
+    
+    XCTAssertEqual(result, 3)
+  }
+  
+  
+  
+  // MARK: - 3.7. Cover a Loop
+  
+  // 1. Assertion I'm confident will fail
+//  func test_commaSeparated_from2to4_shhouldReturnSomething() {
+//    let result = CoveredClass.commaSeparated(from: 2, to: 4)
+//
+//    XCTAssertEqual(result, "FOO")
+//  }
+  
+  
+  // 2. Plug "2,3,4" into the assertion
+  func test_commaSeparated_from2to4_shhouldReturn2345SeparatedByCommas() {
+    let result = CoveredClass.commaSeparated(from: 2, to: 4)
+    
+    XCTAssertEqual(result, "2,3,4")
+  }
+  
+  
+  // 3. Function’s behavior when it loops zero times
+//  func test_commaSeparated_from2to2_shouldReturnsomething() {
+//    let result = CoveredClass.commaSeparated(from: 2, to: 2)
+//
+//    XCTAssertEqual(result, "FOO")
+//  }
+  
+  
+  // 4. Plug "2" into the assertion
+  func test_commaSeparated_from2to2_shouldReturn2WithNoCommas() {
+    let result = CoveredClass.commaSeparated(from: 2, to: 2)
+    
+    XCTAssertEqual(result, "2")
+  }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  
+  
+  // MARK: - 3.8. Cover Statements in a Sequence
+  
+  // 1. Assertion I'm confident will fail
+//  func test_area_withWidth7_shouldBeSomething() {
+//    let sut = CoveredClass()
+//
+//    sut.width = 7
+//
+//    XCTAssertEqual(sut.area, -1)
+//  }
+  
+  
+  // 2. Plug 49 into the assertion
+  func test_area_withWidth7_shouldBe49() {
+    let sut = CoveredClass()
+    
+    sut.width = 7
+    
+    XCTAssertEqual(sut.area, 49)
+  }
+  
+  
+  
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  
+  
+  
+  
+  
+//  func test_zero() {
+//    XCTFail("Tests not yet implemented in CoveredClassTests")
+//  }
 }
