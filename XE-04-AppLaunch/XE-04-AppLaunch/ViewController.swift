a61f876fcc40adcb2860c5033e7244cc0a8027fe
Ignasi  Perez <perezignasi@gmail.com>
2021-01-20 10:25:21 +0100

    XE-04 AppLaunch -created

---
+++ XE-04-AppLaunch/XE-04-AppLaunch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XE-04-AppLaunch
+//
+//  Created by ignasiperez.com on 20/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
