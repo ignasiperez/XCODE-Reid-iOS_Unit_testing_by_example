f582f322bf875ebbd86d72bedc98c513263fcc16
Ignasi  Perez <perezignasi@gmail.com>
2021-01-22 21:16:24 +0100

    XE-05 LoadViewControllers -created

--- XE-04 AppLaunch/MyAppLaunch/MyAppLaunch/main.swift
+++ XE-04 AppLaunch/MyAppLaunch/MyAppLaunch/main.swift
@@ -5,4 +5,13 @@
 //  Created by ignasiperez.com on 21/01/2021.
 //
 
-import Foundation
+import UIKit
+
+let appDelegateClass: AnyClass =
+  NSClassFromString("TestingAppDelegate") ?? AppDelegate.self
+
+UIApplicationMain(CommandLine.argc,
+                  CommandLine.unsafeArgv,
+                  nil,
+                  NSStringFromClass(appDelegateClass))
+
